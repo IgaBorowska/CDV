530efabfb77661cd838831fe7ea2c4e4177cdcd9
Iga Borowska <iborowska@edu.cdv.pl>
2020-03-05 17:21:54 +0100

    Zajecia_1

--- 1_podstawy.py
+++ 1_podstawy.py
@@ -1,1 +1,22 @@
-print("Janusz")
+
+######
+surname="Kowalski"
+firstChar - surname[0]
+print(firstChar)
+
+lenght=len(surname)
+print(lenght)
+
+lastChar=surname[len(surname)-1]
+print(lastChar)
+
+#konwersja
+x="5"
+print(type(x))
+x=int(x)
+print(type(x))
+
+y=4
+print(type(y))
+y=y/2
+print(type(y))
